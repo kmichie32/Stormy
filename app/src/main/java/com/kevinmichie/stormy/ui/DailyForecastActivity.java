23f2c66eb73ed44e9974c29b8be5b98238c7f12f
Kevin Michie <kmichie32@gmail.com>
2016-03-13 20:23:58 -0500

    7 day forecast is working, but struggling to format location
    I am going to try and work on getting my current location and
    reformatting the layout.

--- app/src/main/java/com/kevinmichie/stormy/ui/DailyForecastActivity.java
+++ app/src/main/java/com/kevinmichie/stormy/ui/DailyForecastActivity.java
@@ -1,25 +1,33 @@
 package com.kevinmichie.stormy.ui;
 
 import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 
 import com.kevinmichie.stormy.R;
+import com.kevinmichie.stormy.adapters.DayAdapter;
+import com.kevinmichie.stormy.weather.Day;
+
+import java.util.Arrays;
 
 public class DailyForecastActivity extends ListActivity {
+
+    private Day[] mDays; //place holder for the time being
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daily_forecast);
 
-        String[] daysOfTheWeek = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1,
-                daysOfTheWeek);
+        Intent intent = getIntent();
+        Parcelable[] parcelables = intent.getParcelableArrayExtra(MainActivity.DAILY_FORECAST);
+        mDays = Arrays.copyOf(parcelables, parcelables.length, Day[].class);
 
-        setListAdapter(adapter);
+
+        DayAdapter adapter = new DayAdapter(this, mDays);
     }
 
 }
--- app/src/main/java/com/kevinmichie/stormy/weather/Day.java
+++ app/src/main/java/com/kevinmichie/stormy/weather/Day.java
@@ -1,6 +1,13 @@
 package com.kevinmichie.stormy.weather;
 
-public class Day {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+public class Day implements Parcelable {
 
     private long mTime;
     private String mSummary;
@@ -24,8 +31,8 @@ public class Day {
         mSummary = summary;
     }
 
-    public double getTemperatureMax() {
-        return mTemperatureMax;
+    public int getTemperatureMax() {
+        return (int)Math.round(mTemperatureMax);
     }
 
     public void setTemperatureMax(double temperatureMax) {
@@ -47,4 +54,52 @@ public class Day {
     public void setTimezone(String timezone) {
         mTimezone = timezone;
     }
+
+    public int getIconId(){
+        return Forecast.getIconId(mIcon);
+    }
+
+    public String getDayOfTheWeek(){
+        SimpleDateFormat formatter = new SimpleDateFormat("EEEE");
+        formatter.setTimeZone(TimeZone.getTimeZone(mTimezone));
+        Date dateTime = new Date(mTime * 1000);
+        return formatter.format(dateTime);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeLong(mTime);
+        dest.writeString(mSummary);
+        dest.writeDouble(mTemperatureMax);
+        dest.writeString(mIcon);
+        dest.writeString(mTimezone);
+    }
+
+    private Day(Parcel in) { //has to be in order!
+        mTime = in.readLong();
+        mSummary = in.readString();
+        mTemperatureMax = in.readDouble();
+        mIcon = in.readString();
+        mTimezone = in.readString();
+
+    }
+
+    public Day(){}
+    public static final Creator<Day> CREATOR = new Creator<Day>() {
+        @Override
+        public Day createFromParcel(Parcel source) {
+            return new Day(source);
+        }
+
+        @Override
+        public Day[] newArray(int size) {
+            return new Day[size];
+        }
+    };
+
 }
